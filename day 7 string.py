11c2c0433f0e2b36700aa024012e97f3529dd338
georgepr0xy <99556878+georgepr0xy@users.noreply.github.com>
2023-05-04 01:21:12 +0530

    Create day 7 string.py

---
+++ day 7 string.py
@@ -0,0 +1,16 @@
+name ="george"
+gender ="male"
+print(name)
+st = """ giw hhieffhoijefef'eef
+
+fejkf'fjfjo"""
+print(st)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+#print(name[6]) this throw  an error
+for character in st:
+    print(character)

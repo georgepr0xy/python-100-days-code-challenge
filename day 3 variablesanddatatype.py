2512fef659fc84be51f8c0a001ace508f88027ba
georgepr0xy <99556878+georgepr0xy@users.noreply.github.com>
2023-05-03 10:30:19 +0530

    Create day 3 variablesanddatatype.py

---
+++ day 3 variablesanddatatype.py
@@ -0,0 +1,18 @@
+a=12354545444464
+print(a)
+b="harry"
+print(b)
+a=5
+b="george"
+c=7.5
+d=a+c
+e=complex(5,2)
+print(d)
+print("this is a data type of =", type(a))
+print("this is a data type of =",type(b))
+print("this is a data type of =",type(d))
+print("this is a data type of =",type(e))
+list1=("george",4,[4,5,["apple","orange"]])
+print(list1)
+dict1={"name":"george","age":22,"canVote":True}
+print(dict1)

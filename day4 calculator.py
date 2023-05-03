5789f7f681d684553c2872b874222c65aca67c60
georgepr0xy <99556878+georgepr0xy@users.noreply.github.com>
2023-05-03 10:31:42 +0530

    Create day4 calculator.py

---
+++ day4 calculator.py
@@ -0,0 +1,8 @@
+a=5
+b=6
+print("The value of",a,"+",b, "is", a+b)
+print("The value of",a,"-",b, "is", a-b)
+print("The value of",a,"*",b, "is", a*b)
+print("The value of",a,"/",b, "is", a/b)
+print("The value of",a,"%",b, "is", a%b)
+print("The value of",a,"**",b, "is", a**b)

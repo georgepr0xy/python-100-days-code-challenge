269c36c76bfb12e361412b2856483d091abca856
georgepr0xy <99556878+georgepr0xy@users.noreply.github.com>
2023-05-03 10:29:17 +0530

    Create day2 main.py

---
+++ day2 main.py
@@ -0,0 +1,11 @@
+print("hi")
+print(5)
+print(5)
+# wow this line  act as comment 
+# this is a hundred day of code challenge
+print("i am a good boy \nshe is \'also a good girl\'")
+''' this
+is 
+ a multi line comment
+   '''
+print("hello",4,7, sep="~")

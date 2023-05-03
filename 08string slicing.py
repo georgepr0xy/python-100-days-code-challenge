803e1d912536d2d8e033ddddb5d1b91ecda06d9b
georgepr0xy <99556878+georgepr0xy@users.noreply.github.com>
2023-05-04 01:23:19 +0530

    Add files via upload

---
+++ 08string slicing.py
@@ -0,0 +1,14 @@
+names="harry,subham"
+print(names[0:8])
+print(len(names))
+fruit="mango"
+mangolen=len(fruit)
+print(len(fruit))
+print(mangolen)
+print(fruit[0:5])
+print(fruit[0:-4])
+print(fruit[0:-4])
+print(fruit[-5:-2])
+print(fruit[-3:-2])
+nm = "Harry"
+print(nm[-4:-2])

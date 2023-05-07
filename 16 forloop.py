9c659c98a18e9a1aeb3ee62b50e5ac94998ef065
georgepr0xy <99556878+georgepr0xy@users.noreply.github.com>
2023-05-07 21:02:13 +0530

    Add files via upload

---
+++ 16 forloop.py
@@ -0,0 +1,20 @@
+name = "George"
+for i in name:
+  print(i)
+  if(i == "e"):
+    print("this is something special")
+    
+
+colors = ["red","green","yellow","blue"]
+for i in colors:
+    print(i)
+    for j in i:
+        print(j)
+
+for i in range(9):
+    print(i+1)
+for i in range(1,20001): 
+    print(i)
+
+for k in range(1,2000):
+    print(k)      
---
+++ 17 while.py
@@ -0,0 +1,20 @@
+'''for i in range(9):
+    print(i)
+i=0
+while(i<9):
+    print(i)
+    i=i+1 
+i=int(input("enter the number:"))
+while(i<15):
+    i=int(input("enter the number:"))
+    print(i)
+else:
+    print("number is > 15 so, done with the loop")'''
+
+count=5
+while(count>0):
+    print(count)
+    count=count-1
+
+
+

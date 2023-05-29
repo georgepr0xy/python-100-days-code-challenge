46d18bea7a4ff25eb33ff5eadbbf0c82f367fea2
georgepr0xy <99556878+georgepr0xy@users.noreply.github.com>
2023-05-29 22:42:12 +0530

    Add files via upload

---
+++ 22 list method.py
@@ -0,0 +1,15 @@
+l = [55,66,33,11,35,36]
+print(l)
+#l.append(7)
+#l.sort(reverse=True)
+#l.reverse()
+#print(l.index(33))
+#print(l.count(66))
+#m = l.copy()
+
+#l.insert(5,899)
+#print(l)
+m=["car","bike","truck"]
+#l.extend(m)
+k=m+l
+print(k)
---
+++ 23 operation on tuple.py
@@ -0,0 +1,18 @@
+#countries = ("italy","india","england","germany")
+#temp = list(countries)
+#print(temp)
+#temp.append("russia")
+#print(temp)
+#temp.pop(4)
+#print(temp)
+#temp[2] ="finland"
+#print(temp)
+#countries =tuple(temp)
+#print(countries)
+
+
+tuple1 = (0,1,2,3,4,3,5,3,3,1,4,3,2,5,3,5,2,1,3)
+#res=tuple1.count(3)
+#res = tuple1.index(3,4,17)
+res = len(tuple1)
+print("count of 3 in tuple1 is:", res)

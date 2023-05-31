7adffa5227bc196e7447bf2c708e05b4fe2f2301
georgepr0xy <99556878+georgepr0xy@users.noreply.github.com>
2023-06-01 02:06:34 +0530

    Add files via upload

---
+++ 28 docstring.py
@@ -0,0 +1,6 @@
+def square(n):
+    "this is the docstring not a comment"
+    print(n**2)
+square(5)    
+print(square.__doc__)
+
---
+++ 30 set.py
@@ -0,0 +1,13 @@
+s = {1,5,3,2,3,4,2,4,45,6}
+print(s)
+print(type(s))
+
+
+#how to make an empty set
+s=set()
+print(type(s))
+
+#it is not a empty set
+
+info={}
+print(type(info))

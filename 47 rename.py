2510a345350cddec0af72922a5368e568fe6531e
georgepr0xy <99556878+georgepr0xy@users.noreply.github.com>
2023-07-10 12:20:46 +0530

    Add files via upload

---
+++ 47 rename.py
@@ -0,0 +1,5 @@
+import os 
+
+for i in range (0,100):
+    os.rename(f"data/george{i+1}",f"data/province{i+1}")
+ 
---
+++ 48 oslist.py
@@ -0,0 +1,7 @@
+import os
+folders = os.listdir("data")
+print(folders)
+
+for folder in folders:
+    print(folder)
+    print(os.listdir(f"data/{folder}"))

a2e825200144252d2a2c515ba0a769100456fdd0
George Province <99556878+georgepr0xy@users.noreply.github.com>
2023-07-27 01:26:36 +0530

    Add files via upload

---
+++ 74 method overriding.py
@@ -0,0 +1,24 @@
+class shape:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def area(self):
+        return self.x * self.y
+    
+class circle(shape):
+    def __init__(self,radius):
+        self.radius = radius
+        super().__init__(radius,radius)
+
+    def area(self):
+        return 3.14 * super().area()    
+
+   
+rectangle =shape(4,5)
+print(rectangle.area())
+
+
+cir=circle(5)
+print(cir.area())
+
+

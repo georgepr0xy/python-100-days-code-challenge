60bb3551ce5f5183384fc1181aaab4307eaa21c6
George Province <99556878+georgepr0xy@users.noreply.github.com>
2023-07-30 13:52:05 +0530

    Add files via upload

---
+++ 76 pdf merge.py
@@ -0,0 +1,12 @@
+from PyPDF2 import PdfWriter
+import os
+
+merger= PdfWriter()
+files = [file for file in os.listdir() if file.endswith(".pdf")]
+
+for pdf in files:
+    merger.append(pdf)
+
+merger.write("merger-pdf.pdf")
+merger.close() 
+ 

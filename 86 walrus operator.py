ba7aca9f641e3c29e4fb0a50689b6a9275b05aed
George Province <99556878+georgepr0xy@users.noreply.github.com>
2023-08-01 00:48:25 +0530

    Add files via upload

---
+++ 86 walrus operator.py
@@ -0,0 +1,13 @@
+# numbers =[1,2,3,4,5]
+# while(n:=len(numbers))>0:
+#  print(numbers.pop())
+
+foods = list()
+# while True:
+
+#     food =input("which food would you like to eat : ")
+#     if food == "quit" :
+#         break 
+#     foods.append(food)
+while (food :=input("which food do you like to eat: ")) != "quit":
+    foods.append(food)

2259d70d622690dcc3dd4104debed19237cdb9f7
George Province <99556878+georgepr0xy@users.noreply.github.com>
2023-08-01 00:50:21 +0530

    Add files via upload

---
+++ 88 shoutout.py
@@ -0,0 +1,6 @@
+import shutil
+# import os
+shutil.copy("90 news api.py","generator.py")
+# shutil.copytree("clutteredfolder","newfolder")
+# shutil.move("clutteredfolder/1.png","1.png")
+# os.remove("1.png")
